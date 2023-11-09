ef98744d4adfcb4b1e1c07147d12076cc8120c11
Jeffrey <562475805lanu@gmail.com>
2023-11-09 17:37:59 +1100

    add user model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createEvents: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Event",
+    },
+  ],
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
