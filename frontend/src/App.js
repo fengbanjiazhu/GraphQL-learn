39074ae4929bf077838f9ae9b8ad9d06887642cd
Jeffrey <562475805lanu@gmail.com>
2023-11-16 17:47:19 +1100

    init front end

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ height: "100%", width: "100%", textAlign: "center" }} className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
