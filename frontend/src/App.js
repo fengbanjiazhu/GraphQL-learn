82d4f6625b98f7c46be030850423e4c0b3221cb1
Jeffrey <562475805lanu@gmail.com>
2023-11-16 17:54:02 +1100

    add routes and init 2 pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <div style={{ height: "100%", width: "100%", textAlign: "center" }} className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
